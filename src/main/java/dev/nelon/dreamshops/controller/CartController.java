5f78824a7bb178d8a5ba6ef378b72861d002ddc5
NelonArrive <nelon.arrive@gmail.com>
2025-12-17 19:27:30 +0500

    feat: add CartItemController with endpoints for adding, removing, and updating items in the cart; implement initializeNewCart method in CartService

--- src/main/java/dev/nelon/dreamshops/controller/CartController.java
+++ src/main/java/dev/nelon/dreamshops/controller/CartController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
@@ -26,7 +28,23 @@ public class CartController {
 		}
 	}
 	
+	@DeleteMapping("/{cartId}/clear")
 	public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId) {
-		cartI
+		try {
+			cartService.clearCart(cartId);
+			return ResponseEntity.ok(new ApiResponse("Clear Cart Success!", null));
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+	}
+	
+	@GetMapping("/{cartId}/cart/total-price")
+	public ResponseEntity<ApiResponse> getTotalAmount(@PathVariable Long cartId) {
+		try {
+			BigDecimal totalPrice = cartService.getTotalPrice(cartId);
+			return ResponseEntity.ok(new ApiResponse("Total Price", totalPrice));
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
 	}
 }
